dfdf4d8c58fdde3fa8579cd74bd133e3159516aa
Diego Briceño <diegobriceno13@gmail.com>
2021-01-07 21:31:14 +0000

    Dados corretos e incorretos

--- Projeto ER/js/login.js
+++ Projeto ER/js/login.js
@@ -4,20 +4,31 @@ function login() {
     var pass = document.getElementById("password").value;
     var query = "SELECT * FROM user WHERE username='" + username + "' and password='" + pass + "'";
     connection.query(query, function(err, result, fields) {
-
+    
         if (err) {
             console.log("Erro na query.");
         }
         else{
             if (result.length == 0) {
-                alert("Os dados não estão corretos.")
-                window.location.replace("../html/login.html")
+                $("#dadosIncorretos").fadeTo(2000, 500).slideUp(500, function(){
+                    $("#dadosIncorretos").slideUp(500);
+                });
+                
+                setTimeout(function () {
+                    window.location.replace("../html/login.html")
+                }, 3000);
+                
             } else {
                 sessionStorage.setItem("idUser", result[0].id);
                 sessionStorage.setItem("username", result[0].username);
                 sessionStorage.setItem("typeUser", result[0].user_type);
-                window.location.replace("../html/dashboard_"+result[0].user_type+".html")
-                alert("Login efetuado.")
+                
+                $("#dadosCorretos").fadeTo(2000, 500).slideUp(500, function(){
+                    $("#dadosCorretos").slideUp(500);
+                });
+                setTimeout(function () {
+                    window.location.replace("../html/dashboard_"+result[0].user_type+".html")
+                }, 3000);
             }
         }
     });
