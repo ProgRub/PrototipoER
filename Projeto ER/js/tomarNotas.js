9829956222c1e61863c3be4517e7ecd5302f7148
orlandoxnica <58520368+orlandoxnica@users.noreply.github.com>
2021-01-24 14:21:23 +0000

    Tomar Notas
    Continuaçao tomar notas

--- Projeto ER/js/tomarNotas.js
+++ Projeto ER/js/tomarNotas.js
@@ -1,12 +1,12 @@
+//INSERE NOTAS NA BD:
 function tomarNotas(nota){
-    
     connectDataBase();
     var id_explicandor = sessionStorage.getItem("idUser");
     var id_disciplina = sessionStorage.getItem("disciplina_id");
-    //var id_explicando = sessionStorage.getItem("idExplicando");
+    var id_explicando = sessionStorage.getItem("aluno_id");
 
     setTimeout(function(){
-    query = "UPDATE explicando_tem_explicador SET notas = '"+nota+"' WHERE explicando_user_id = '4' AND explicador_user_id = '"+id_explicandor+"' AND disciplina_id = '"+id_disciplina+"';";
+    query = "UPDATE explicando_tem_explicador SET notas = '"+nota+"' WHERE explicando_user_id = '"+id_explicando+"' AND explicador_user_id = '"+id_explicandor+"' AND disciplina_id = '"+id_disciplina+"';";
         connection.query(query, function (err, result, fields) {
             if (err) {
                 console.log(err);
@@ -21,9 +21,9 @@ function tomarNotas(nota){
 
 //listar alunos do explicando
 function listarAlunosExplicando(){
-
-    var id_explicandor = sessionStorage.getItem("idUser");
-    query = "SELECT explicando_user_id, explicador_user_id FROM explicando_tem_explicador WHERE explicador_user_id= '"+id_explicandor+"'";
+    let idsExplicandos = [];
+    var id_explicador = sessionStorage.getItem("idUser");
+    query = "SELECT explicando_user_id, explicador_user_id FROM explicando_tem_explicador WHERE explicador_user_id= '"+id_explicador+"'";
     console.log(query);
     connectDataBase();
 
@@ -32,73 +32,106 @@ function listarAlunosExplicando(){
           console.log(err);
         } else {
             result.forEach(explicando_tem_explicador => {
-                console.log(explicando_tem_explicador);
-                var opcao = document.createElement("option");
-                opcao.value = explicando_tem_explicador.explicando_user_id;
-                select.appendChild(opcao);
+              idsExplicandos.push(explicando_tem_explicador.explicando_user_id);
+              console.log("CERTO");
             });
-
-
-
         }
+        query = "SELECT * FROM explicando";
+        console.log(query);
+        connection.query(query, function (err, result) {
+          if (err) {
+            console.log(err);
+          } else {
+            var select = document.createElement("select");
+            select.setAttribute("class", "form-control");
+            select.id = "alunoEscolhido";
+            select.size = 5;
+            document.getElementById("escolherAluno").appendChild(select);
+      
+            result.forEach((explicando) => {
+              if(idsExplicandos.includes(explicando.user_id)){
+                console.log(explicando);
+                var opcao = document.createElement("option");
+                opcao.value = explicando.user_id;
+                opcao.innerHTML = explicando.nome;
+                select.appendChild(opcao);
+              } 
+            });
+      
+            paragrafo("escolherAluno");
+            paragrafo("escolherAluno");
+      
+            var a = document.createElement("a");
+            var link = document.createTextNode("Continuar");
+            a.appendChild(link);
+            a.title = "Continuar";
+            a.setAttribute("class", "btn btn-primary");
+            a.href = "recursoExtra.html";
+            a.onclick = function() {
+              var x = document.getElementById("alunoEscolhido").value;
+              var y = select.options[select.selectedIndex].text;
+              sessionStorage.setItem("aluno_id", x);
+              sessionStorage.setItem("aluno_nome", y);
+            };
+            console.log(a);
+            document.getElementById("escolherAluno").appendChild(a);
+            
+          }
+        });
       });
-
-
-
       closeConnectionDataBase();
 }
 
+function paragrafo(elemento){
+  var paragrafo = document.createElement("br");
+      document.getElementById(elemento).appendChild(paragrafo);
+}
 
 
-//LISTA DISCIPLINAS DO ALUNO:
 function listarDisciplinas(){
-    var id = sessionStorage.getItem("idUser");
-    query = "SELECT id, nome FROM disciplina WHERE explicador_user_id = '"+ id +"'";
-    console.log(query);
-    connectDataBase();
-  
-    connection.query(query, function (err, result) {
-      if (err) {
-        console.log(err);
-      } else {
-        var select = document.createElement("select");
-        select.setAttribute("class", "form-control");
-        select.id = "disciplinaEscolhida";
-        select.size = 5;
-        document.getElementById("escolherDisciplina").appendChild(select);
-  
-        result.forEach((disciplina) => {
-          console.log(disciplina);
-          var opcao = document.createElement("option");
-          opcao.value = disciplina.id;
-          opcao.innerHTML = disciplina.nome;
-          select.appendChild(opcao);
-        });
-  
-        paragrafo("escolherDisciplina");
-        paragrafo("escolherDisciplina");
-  
-        var a = document.createElement("a");
-        var link = document.createTextNode("Continuar");
-        a.appendChild(link);
-        a.title = "Continuar";
-        a.setAttribute("class", "btn btn-primary");
-        a.href = "recursoExtra.html";
-        a.onclick = function() {
-          var x = document.getElementById("disciplinaEscolhida").value;
-          var y = select.options[select.selectedIndex].text;
-          sessionStorage.setItem("disciplina_id", x);
-          sessionStorage.setItem("disciplina_nome", y);
-        };
-        console.log(a);
-        document.getElementById("escolherDisciplina").appendChild(a);
-        
-      }
-    });
-    closeConnectionDataBase();
-  }
+  var id = sessionStorage.getItem("idUser");
+  query = "SELECT id, nome FROM disciplina WHERE explicador_user_id = '"+ id +"'";
+  console.log(query);
+  connectDataBase();
 
-  function paragrafo(elemento){
-    var paragrafo = document.createElement("br");
-        document.getElementById(elemento).appendChild(paragrafo);
-  }
+  connection.query(query, function (err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      var select = document.createElement("select");
+      select.setAttribute("class", "form-control");
+      select.required;
+      select.id = "disciplinaEscolhida";
+      select.size = 5;
+      document.getElementById("escolherDisciplina").appendChild(select);
+
+      result.forEach((disciplina) => {
+        console.log(disciplina);
+        var opcao = document.createElement("option");
+        opcao.value = disciplina.id;
+        opcao.innerHTML = disciplina.nome;
+        select.appendChild(opcao);
+      });
+
+      paragrafo("escolherDisciplina");
+      paragrafo("escolherDisciplina");
+
+      var a = document.createElement("a");
+      var link = document.createTextNode("Continuar");
+      a.appendChild(link);
+      a.title = "Continuar";
+      a.setAttribute("class", "btn btn-primary");
+      a.href = "tomarNotasEscolheAluno.html";
+      a.onclick = function() {
+        var x = document.getElementById("disciplinaEscolhida").value;
+        var y = select.options[select.selectedIndex].text;
+        sessionStorage.setItem("disciplina_id", x);
+        sessionStorage.setItem("disciplina_nome", y);
+      };
+      console.log(a);
+      document.getElementById("escolherDisciplina").appendChild(a);
+      
+    }
+  });
+  closeConnectionDataBase();
+}
